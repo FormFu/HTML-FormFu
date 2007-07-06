adeea570c5b050d09be42bb459a9db04effe527d
fireartist <fireartist@gmail.com>
2007-07-06 11:56:22 +0000

    adding benchmark file

---
+++ benchmarks/benchmark.pl
@@ -0,0 +1,100 @@
+use strict;
+use warnings;
+use lib 'lib';
+use HTML::FormFu;
+use HTML::Widget;
+use CGI::FormBuilder;
+use Benchmark qw( cmpthese );
+
+my $formfu  = formfu();
+my $widget  = widget();
+my $builder = builder();
+
+#print $formfu;
+#print "\n\n**********\n\n";
+#print $widget->process;
+#print "\n\n**********\n\n";
+#print $builder->render;
+
+# make sure TT has loaded/cached everything
+my $output = "$formfu";
+
+cmpthese(
+    500,
+    {
+        'HTML::FormFu' => sub {
+            $output = "$formfu";
+        },
+        'HTML::Widget' => sub {
+            $output = $widget->process->as_xml;
+        },
+        'CGI::FormBuilder' => sub {
+            $output = $builder->render;
+        },
+    }
+);
+
+sub formfu {
+    my $form = HTML::FormFu->new;
+    
+    $form->auto_fieldset(1);
+    
+    for (1..10) {
+        $form->element({
+            name  => "text$_",
+            size  => 10,
+            label => "text & $_",
+        });
+    }
+    
+    $form->element({
+        name    => "select",
+        type    => "select",
+        label   => "select",
+        values  => [ 1907 .. 2007 ],
+        default => 2007,
+    });
+    
+    $form->element({
+        type => "submit",
+        name => "submit",
+    });
+    
+    return $form;
+}
+
+sub widget {
+    my $form = HTML::Widget->new;
+    
+    for (1..10) {
+        $form->element( "Textfield", "text$_" )->label("text & $_")->size(10);
+    }
+    
+    $form->element( "Select", "select" )->label("select")->options(
+        map { $_, $_ } 1907 .. 2007 )->selected(2007);
+    
+    $form->element( "Submit", "submit" );
+    
+    return $form;
+}
+
+sub builder {
+    my $form = CGI::FormBuilder->new;
+    
+    for (1..10) {
+        $form->field(
+            name  => "text$_",
+            size  => 10,
+            label => "text & $_",
+        );
+    }
+    
+    $form->field(
+        name    => "select",
+        options => [ 1907 .. 2007 ],
+        value   => 2007,
+        
+    );
+    
+    return $form;
+}
