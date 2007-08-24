7482c78aa95abb682ccc4815533f7cf50b4ea274
fireartist <fireartist@gmail.com>
2007-08-24 08:39:58 +0000

    updated vertically-aligned example to work with latest code

--- examples/vertically-aligned/vertically-aligned.pl
+++ examples/vertically-aligned/vertically-aligned.pl
@@ -1,14 +1,17 @@
 use strict;
 use warnings;
+use lib 'lib';
 use HTML::FormFu;
 use Template;
 
-my $form = HTML::FormFu->new->load_config_file('examples/vertically-aligned.yml');
+my $form = HTML::FormFu->new;
+
+$form->load_config_file('examples/vertically-aligned/vertically-aligned.yml');
 
 my $tt = Template->new;
 
 $tt->process(
-    'examples/vertically-aligned.tt',
+    'examples/vertically-aligned/vertically-aligned.tt',
     { form => $form },
-    'examples/vertically-aligned.html',
+    'examples/vertically-aligned/vertically-aligned.html',
 ) || die $tt->error;
