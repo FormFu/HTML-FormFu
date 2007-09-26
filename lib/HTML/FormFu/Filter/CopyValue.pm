2de4dcbfd6fd9f6efbde5ba453d3bd88e00d9a78
mariominati <mario.minati@googlemail.com>
2007-09-26 15:26:08 +0000

    new CopyValue Filter tests and docs are included

---
+++ lib/HTML/FormFu/Filter/CopyValue.pm
@@ -0,0 +1,67 @@
+package HTML::FormFu::Filter::CopyValue;
+
+use strict;
+use base 'HTML::FormFu::Filter';
+
+__PACKAGE__->mk_accessors(qw/ field /);
+
+sub filter {
+    my ( $self, $value ) = @_;
+
+    return $value
+        if (defined $value && length $value);
+
+    my $field_name = $self->field
+        or die "Parameter 'field' is not defined.";
+
+    my $parent = $self->parent
+        or die "Can't determine my parent.";
+
+    my $field_value = $parent->form->input->{ $field_name };
+
+    return $field_value;
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+HTML::FormFu::Filter::CopyValue - copy the value from an other field
+
+=head1 SYNOPSIS
+
+   elements:
+      - type: Text
+        name: username
+      - type: Text
+        name: nickname
+        filters:
+           - type: CopyValue
+             field: username
+
+=head1 DESCRIPTION
+
+Filter copying the value of another field if the original value of this field
+is empty.
+
+=head1 CAVEATS
+
+If the value of the original field contains an invalid value (a value that
+will be constrained through a constraint) this invalid value will be choosen
+for this field (the field with CopyValue filter).
+
+So the user has to change two fields or you remove the invalid value in a
+custom constraint.
+
+=head1 AUTHOR
+
+Mario Minati, C<mario.minati@googlemail.com>
+
+=head1 LICENSE
+
+This library is free software, you can redistribute it and/or modify it under
+the same terms as Perl itself.
+
+=cut
---
+++ t/filters/copyvalue.t
@@ -0,0 +1,30 @@
+use strict;
+use warnings;
+
+use Test::More tests => 4;
+
+use HTML::FormFu;
+
+my $form = HTML::FormFu->new;
+
+$form->element('Text')->name('foo');
+$form->element('Text')->name('bar')->filter('CopyValue')->field('foo');
+
+my $original_foo = "ab123456";
+my $filtered_foo = "ab123456";
+
+my $filtered_bar = $filtered_foo;
+
+$form->process( {
+        foo => $original_foo,
+        bar => undef,
+    } );
+
+# foo is quoted
+is( $form->param('foo'), $filtered_foo, 'foo filtered' );
+is( $form->params->{foo}, $filtered_foo, 'foo filtered' );
+
+# bar is filtered
+is( $form->param('bar'), $filtered_bar, 'bar filtered' );
+is( $form->params->{bar}, $filtered_bar, 'bar filtered' );
+
