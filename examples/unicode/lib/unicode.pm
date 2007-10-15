58234601690db730eeb0709af64e4dbad94c9956
fireartist <fireartist@gmail.com>
2007-10-15 12:32:23 +0000

    added Plugin::Unicode updated prereqs

--- examples/unicode/lib/unicode.pm
+++ examples/unicode/lib/unicode.pm
@@ -13,7 +13,7 @@ use Catalyst::Runtime '5.70';
 # Static::Simple: will serve static files from the application's root 
 #                 directory
 
-use Catalyst qw/-Debug ConfigLoader Static::Simple/;
+use Catalyst qw/-Debug ConfigLoader Static::Simple Unicode /;
 
 our $VERSION = '0.01';
 
