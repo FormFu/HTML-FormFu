6167d75267d285388629ba8a8d78606ef13a7046
fireartist <fireartist@gmail.com>
2007-10-30 12:06:42 +0000

    updated tt_files.pm

--- lib/HTML/FormFu/tt_files.pm
+++ lib/HTML/FormFu/tt_files.pm
@@ -30,18 +30,18 @@ __CPAN_HTML_FormFu__label__
 <span[% process_attrs(self.attributes) %]>
 [% FOREACH option = self.options %][% IF option.group %]<span[% process_attrs(option.attributes) %]>
 [% FOREACH item = option.group %]<span>
-<input name="[% self.name %]" type="radio" value="[% item.value %]"[% process_attrs(item.attributes) %] />
+<input name="[% self.nested_name %]" type="radio" value="[% item.value %]"[% process_attrs(item.attributes) %] />
 <label[% process_attrs(item.label_attributes) %]>[% item.label %]</label>
 </span>
 [% END %]</span>
 [% ELSE %]<span>
-<input name="[% self.name %]" type="radio" value="[% option.value %]"[% process_attrs(option.attributes) %] />
+<input name="[% self.nested_name %]" type="radio" value="[% option.value %]"[% process_attrs(option.attributes) %] />
 <label[% process_attrs(option.label_attributes) %]>[% option.label %]</label>
 </span>
 [% END %][% END %]</span>__CPAN_HTML_FormFu__multi_ltr__
 [% IF self.label.defined %][% INCLUDE $self.label_filename %]
 [% END %][% INCLUDE $self.field_filename %]__CPAN_HTML_FormFu__content_button__
-[% WRAPPER field %]<button name="[% self.name %]" type="[% self.field_type %]"[% IF self.value.defined %] value="[% self.value %]"[% END %][% process_attrs(self.attrs) %]>[% self.content %]</button>[% END %]__CPAN_HTML_FormFu__start_block__
+[% WRAPPER field %]<button name="[% self.nested_name %]" type="[% self.field_type %]"[% IF self.value.defined %] value="[% self.value %]"[% END %][% process_attrs(self.attrs) %]>[% self.content %]</button>[% END %]__CPAN_HTML_FormFu__start_block__
 [% IF self.tag.defined %]<[% self.tag %][% process_attrs(self.attrs) %]>[% END %][% IF self.legend.defined %]
 <legend>[% self.legend %]</legend>[% END %]__CPAN_HTML_FormFu__field__
 [% IF self.container_tag.defined %]<[% self.container_tag %][% process_attrs(self.container_attributes) %]>[% END %][% IF self.label.defined && self.label_filename == 'legend' %]
@@ -61,8 +61,8 @@ __CPAN_HTML_FormFu__label__
 [% WRAPPER field %]<span[% process_attrs(self.attributes) %]>
 [% FOREACH element = self.elements %][% element.multi %]
 [% END %]</span>[% END %]__CPAN_HTML_FormFu__input_tag__
-<input[% IF self.name.defined %] name="[% self.name %]"[% END %] type="[% self.field_type %]"[% IF self.value.defined %] value="[% self.value %]"[% END %][% process_attrs(self.attrs) %] />__CPAN_HTML_FormFu__textarea_tag__
-<textarea name="[% self.name %]"[% process_attrs(self.attrs) %]>[% self.value IF self.value.defined %]</textarea>__CPAN_HTML_FormFu__hidden__
+<input[% IF self.nested_name.defined %] name="[% self.nested_name %]"[% END %] type="[% self.field_type %]"[% IF self.value.defined %] value="[% self.value %]"[% END %][% process_attrs(self.attrs) %] />__CPAN_HTML_FormFu__textarea_tag__
+<textarea name="[% self.nested_name %]"[% process_attrs(self.attrs) %]>[% self.value IF self.value.defined %]</textarea>__CPAN_HTML_FormFu__hidden__
 [% INCLUDE $self.field_filename %]__CPAN_HTML_FormFu__end_block__
 [% IF self.tag.defined %]</[% self.tag %]>[% END %]__CPAN_HTML_FormFu__non_block__
 <[% self.tag %][% process_attrs(self.attrs) %] />__CPAN_HTML_FormFu__blank__
@@ -70,7 +70,7 @@ __CPAN_HTML_FormFu__form_error_message__
 [% IF self.force_error_message || self.form_error_message.defined && self.form.has_errors %]
 <div class="form_error_message">[% self.form_error_message %]</div>[% END %]__CPAN_HTML_FormFu__end_form__
 </form>__CPAN_HTML_FormFu__select_tag__
-<select name="[% self.name %]"[% process_attrs(self.attrs) %]>
+<select name="[% self.nested_name %]"[% process_attrs(self.attrs) %]>
 [% FOREACH option = self.options %][% IF option.group %]<optgroup[% IF option.label.defined %] label="[% option.label %]"[% END %][% process_attrs(option.attributes) %]>
 [% FOREACH item = option.group %]<option value="[% item.value %]"[% process_attrs(item.attributes) %]>[% item.label %]</option>
 [% END %]</optgroup>
