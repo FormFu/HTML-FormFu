1fa8cdde37323c42724fcf0a114f9e1460dbf9de
bricas <brian.cassidy@gmail.com>
2007-11-16 16:11:06 +0000

    first stab at checkboxgroup element. hurrah for copy+paste. :)

---
+++ lib/HTML/FormFu/Element/Checkboxgroup.pm
@@ -0,0 +1,202 @@
+package HTML::FormFu::Element::Checkboxgroup;
+
+use strict;
+use base 'HTML::FormFu::Element::_Group';
+use Class::C3;
+
+use HTML::FormFu::Util qw( append_xml_attribute );
+
+__PACKAGE__->mk_accessors(qw/ checkboxgroup_filename /);
+
+sub new {
+    my $self = shift->next::method(@_);
+
+    $self->filename('checkboxgroup');
+    $self->checkboxgroup_filename('checkboxgroup_tag');
+    $self->label_filename('legend');
+    $self->container_tag('fieldset');
+
+    return $self;
+}
+
+sub prepare_id {
+    my ( $self, $render ) = @_;
+
+    my $form_id    = defined $self->form->id ? $self->form->id : '';
+    my $field_name = defined $self->name     ? $self->name     : '';
+    my $count      = 0;
+
+    for my $option ( @{ $render->{options} } ) {
+        if ( exists $option->{group} ) {
+            for my $item ( @{ $option->{group} } ) {
+                $self->_prepare_id( $item, $form_id, $field_name, \$count );
+            }
+        }
+        else {
+            $self->_prepare_id( $option, $form_id, $field_name, \$count );
+        }
+    }
+
+    return;
+}
+
+sub _prepare_id {
+    my ( $self, $option, $form_id, $field_name, $count_ref ) = @_;
+
+    if ( !exists $option->{attributes}{id} && defined $self->auto_id ) {
+        my %string = (
+            f => $form_id,
+            n => $field_name,
+        );
+
+        my $id = $self->auto_id;
+        $id =~ s/%([fn])/$string{$1}/g;
+        $id =~ s/%c/ ++$$count_ref /gex;
+        
+        if ( defined( my $count = $self->repeatable_count ) ) {
+            $id =~ s/%r/$count/g;
+        }
+
+        $option->{attributes}{id} = $id;
+    }
+
+    # label "for" attribute
+    if (   exists $option->{label}
+        && exists $option->{attributes}{id}
+        && !exists $option->{label_attributes}{for} )
+    {
+        $option->{label_attributes}{for} = $option->{attributes}{id};
+    }
+
+    return;
+}
+
+sub _prepare_attrs {
+    my ( $self, $submitted, $value, $default, $option ) = @_;
+
+    if (   $submitted
+        && defined $value
+        && (ref $value eq 'ARRAY'
+            ? grep { $_ eq $option->{value} } @$value
+            : $value eq $option->{value} ) )
+    {
+        $option->{attributes}{checked} = 'checked';
+    }
+    elsif ($submitted
+        && $self->retain_default
+        && ( !defined $value || $value eq "" )
+        && $self->value eq $option->{value} )
+    {
+        $option->{attributes}{checked} = 'checked';
+    }
+    elsif ($submitted) {
+        delete $option->{attributes}{checked};
+    }
+    elsif ( defined $default
+        && (ref $default eq 'ARRAY'
+            ? grep { $_ eq $option->{value} } @$default
+            : $default eq $option->{value} ) ) {
+        $option->{attributes}{checked} = 'checked';
+    }
+    return;
+}
+
+sub _render_label {
+    my ( $self, $render ) = @_;
+
+    $self->next::method($render);
+
+    if ( defined $render->{label} && $render->{label_filename} eq 'legend' ) {
+        $render->{container_attributes}{class} =~ s/\blabel\b/legend/;
+    }
+
+    return;
+}
+
+sub render {
+    my $self = shift;
+
+    my $render = $self->next::method( {
+            checkboxgroup_filename => $self->checkboxgroup_filename,
+            @_ ? %{ $_[0] } : () } );
+
+    for my $item ( @{ $render->{options} } ) {
+        append_xml_attribute( $item->{attributes}, 'class', 'subgroup' )
+            if exists $item->{group};
+    }
+
+    return $render;
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+HTML::FormFu::Element::Checkboxgroup - Group of checkbox form fields
+
+=head1 SYNOPSIS
+
+YAML config:
+
+    ---
+    elements:
+      - type: Checkboxgroup
+        name: subjects
+        options:
+          - 'Math'
+          - 'Science'
+          - 'English'
+
+=head1 DESCRIPTION
+
+Convenient to use group of checkbox fields.
+
+Use the same syntax as you would to create a Select element optgroup to 
+create Checkboxgroup sub-groups, see L<HTML::FormFu::Element::_Group/options> 
+for details.
+
+=head1 METHODS
+
+=head2 options
+
+See L<HTML::FormFu::Element::_Group/options>.
+
+=head2 values
+
+See L<HTML::FormFu::Element::_Group/values>.
+
+=head2 value_range
+
+See L<HTML::FormFu::Element::_Group/value_range>.
+
+=head2 auto_id
+
+In addition to the substitutions documented by L<HTML::FormFu/auto_id>, 
+C<%c> will be replaced by an incremented integer, to ensure there are 
+no duplicated ID's.
+
+    ---
+    elements:
+      type: Checkboxgroup
+      name: foo
+      auto_id: "%n_%c"
+
+=head1 SEE ALSO
+
+Is a sub-class of, and inherits methods from 
+L<HTML::FormFu::Element::_Group>, 
+L<HTML::FormFu::Element::_Field>, 
+L<HTML::FormFu::Element>
+
+L<HTML::FormFu::FormFu>
+
+=head1 AUTHOR
+
+Carl Franks, C<cfranks@cpan.org>
+
+=head1 LICENSE
+
+This library is free software, you can redistribute it and/or modify it under
+the same terms as Perl itself.
---
+++ t/elements/checkboxgroup.t
@@ -0,0 +1,114 @@
+use strict;
+use warnings;
+
+use Test::More tests => 4;
+
+use HTML::FormFu;
+
+my $form = HTML::FormFu->new({ render_class_args => { INCLUDE_PATH => 'share/templates/tt/xhtml' } });
+
+my $field = $form->element('Checkboxgroup')->name('foo')->value(2)
+    ->options( [ [ 1 => 'One' ], [ 2 => 'Two' ] ] );
+
+# add more elements to test accessor output
+$form->element('Checkboxgroup')->name('foo2')->options( [
+        { label => 'Ein',  value => 1 },
+        { label => 'Zwei', value => 2, attributes => { class => 'foobar' } },
+    ] );
+$form->element('Checkboxgroup')->name('bar')->values( [qw/ one two three /] )
+    ->value('two')->label('My Bar');
+
+my $field_xhtml = qq{<fieldset class="checkboxgroup">
+<span>
+<span>
+<input name="foo" type="checkbox" value="1" />
+<label>One</label>
+</span>
+<span>
+<input name="foo" type="checkbox" value="2" checked="checked" />
+<label>Two</label>
+</span>
+</span>
+</fieldset>};
+
+is( "$field", $field_xhtml );
+
+my $form_xhtml = <<EOF;
+<form action="" method="post">
+$field_xhtml
+<fieldset class="checkboxgroup">
+<span>
+<span>
+<input name="foo2" type="checkbox" value="1" />
+<label>Ein</label>
+</span>
+<span>
+<input name="foo2" type="checkbox" value="2" class="foobar" />
+<label>Zwei</label>
+</span>
+</span>
+</fieldset>
+<fieldset class="checkboxgroup legend">
+<legend>My Bar</legend>
+<span>
+<span>
+<input name="bar" type="checkbox" value="one" />
+<label>One</label>
+</span>
+<span>
+<input name="bar" type="checkbox" value="two" checked="checked" />
+<label>Two</label>
+</span>
+<span>
+<input name="bar" type="checkbox" value="three" />
+<label>Three</label>
+</span>
+</span>
+</fieldset>
+</form>
+EOF
+
+is( "$form", $form_xhtml );
+
+# With mocked basic query
+{
+    $form->process( {
+            foo => [ 1, 2, ],
+            bar => 'three',
+        } );
+
+    my $foo_xhtml = qq{<fieldset class="checkboxgroup">
+<span>
+<span>
+<input name="foo" type="checkbox" value="1" checked="checked" />
+<label>One</label>
+</span>
+<span>
+<input name="foo" type="checkbox" value="2" checked="checked" />
+<label>Two</label>
+</span>
+</span>
+</fieldset>};
+
+    is( $form->get_field('foo'), $foo_xhtml );
+
+    my $bar_xhtml = qq{<fieldset class="checkboxgroup legend">
+<legend>My Bar</legend>
+<span>
+<span>
+<input name="bar" type="checkbox" value="one" />
+<label>One</label>
+</span>
+<span>
+<input name="bar" type="checkbox" value="two" />
+<label>Two</label>
+</span>
+<span>
+<input name="bar" type="checkbox" value="three" checked="checked" />
+<label>Three</label>
+</span>
+</span>
+</fieldset>};
+
+    is( $form->get_field('bar'), $bar_xhtml );
+}
