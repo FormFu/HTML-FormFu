5dd7e6df108c43f2558de55d9d4cd3352004711f
fireartist <fireartist@gmail.com>
2008-03-12 10:05:04 +0000

    new Split filter to split a single input value into an arrayref

---
+++ lib/HTML/FormFu/Filter/Split.pm
@@ -0,0 +1,63 @@
+package HTML::FormFu::Filter::Split;
+
+use strict;
+use base 'HTML::FormFu::Filter';
+
+__PACKAGE__->mk_accessors(qw/ regex limit /);
+
+sub filter {
+    my ( $self, $value ) = @_;
+
+    return if !defined $value;
+
+    my $regex = $self->regex;
+    my $limit = $self->limit || 0;
+
+    $regex = ''if !defined $regex;
+
+    my @values = split /$regex/, $value, $limit;
+
+    return \@values;
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+HTML::FormFu::Filter::Split
+
+=head1 SYNOPSIS
+
+    type: Split
+    regex: '-'
+
+=head1 DESCRIPTION
+
+Split a single input value into an arrayref of values.
+
+=head1 METHODS
+
+=head2 regex
+
+A regex object or string to be passed as the C<PATTERN> argument to C<split>.
+
+Default Value: '' (emtpy string)
+
+=head2 limit
+
+A number passed as the C<LIMIT> argument to C<split>.
+
+Default Value: 0
+
+=head1 AUTHOR
+
+Carl Franks, C<cfranks@cpan.org>
+
+=head1 LICENSE
+
+This library is free software, you can redistribute it and/or modify it under
+the same terms as Perl itself.
+
+=cut
