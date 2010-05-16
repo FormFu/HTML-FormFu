0b981ab21c78cd2006c0a92daebd33466b80607a
fireartist <fireartist@gmail.com>
2010-05-16 16:33:18 +0000

    updated version no. for cpan release

--- lib/HTML/FormFu.pm
+++ lib/HTML/FormFu.pm
@@ -111,7 +111,7 @@ __PACKAGE__->mk_inherited_merging_accessors(qw( tt_args config_callback ));
 *plugins           = \&plugin;
 *add_plugins       = \&add_plugin;
 
-our $VERSION = '0.06001';
+our $VERSION = '0.07000';
 $VERSION = eval $VERSION;
 
 Class::C3::initialize();
