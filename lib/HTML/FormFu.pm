1d92d8312b83fcac274519ceb81562e93a2b33db
Moritz Onken <onken@netcubed.de>
2012-01-23 23:29:46 +0100

    release

--- lib/HTML/FormFu.pm
+++ lib/HTML/FormFu.pm
@@ -143,7 +143,7 @@ __PACKAGE__->mk_inherited_merging_accessors(qw( tt_args config_callback ));
 *plugins           = \&plugin;
 *add_plugins       = \&add_plugin;
 
-our $VERSION = '0.09006';
+our $VERSION = '0.09007';
 $VERSION = eval $VERSION;
 
 sub BUILD {
