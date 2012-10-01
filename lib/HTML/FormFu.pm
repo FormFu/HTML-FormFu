96ab2e1f60be75ebc065742a99649f8b3ddf90bb
fireartist <fireartist@gmail.com>
2012-10-01 08:45:04 +0000

    bumped version number after failed cpan upload

--- lib/HTML/FormFu.pm
+++ lib/HTML/FormFu.pm
@@ -149,7 +149,7 @@ __PACKAGE__->mk_inherited_merging_accessors(qw( tt_args config_callback ));
 *plugins           = \&plugin;
 *add_plugins       = \&add_plugin;
 
-our $VERSION = '0.09008';
+our $VERSION = '0.09009';
 $VERSION = eval $VERSION;
 
 our $build_defaults = {
